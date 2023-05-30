1187cf459a6134e8a5812872652f75e5a3554c07
0xRektora <13756173+0xRektora@users.noreply.github.com>
2023-05-30 10:48:53 -0400

    feat: new `ERC721Mock`

---
+++ contracts/ERC721Mock.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract ERC721Mock is ERC721 {
+    uint256 private _tokenIds;
+
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC721(_name, _symbol) {}
+
+    function mint(address to) public returns (uint256) {
+        _tokenIds += 1;
+        _mint(to, _tokenIds);
+        return _tokenIds;
+    }
+}
