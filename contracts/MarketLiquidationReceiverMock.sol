aa750d7927af537d7ab384dd0820955d7af7c220
cryptotechmaker <grigore.cosmin@gmail.com>
2023-10-04 10:03:52 +0300

    CU-85ztxzc13: added MarketLiquidationReceiverMock

---
+++ contracts/MarketLiquidationReceiverMock.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@boringcrypto/boring-solidity/contracts/interfaces/IERC20.sol";
+import "@boringcrypto/boring-solidity/contracts/libraries/BoringERC20.sol";
+
+contract MarketLiquidationReceiverMock {
+    using BoringERC20 for IERC20;
+
+    IERC20 public asset;
+
+    constructor(IERC20 _token) {
+        asset = _token;
+    }
+
+    function onCollateralReceiver(
+        address,
+        address,
+        uint256,
+        uint256 amountToReceive,
+        bytes calldata
+    ) external returns (bool) {
+        asset.safeTransfer(msg.sender, amountToReceive);
+        return true;
+    }
+}
