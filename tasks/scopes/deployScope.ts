aa375cc6fbc88603a71fb822f02d6524ec8deb86
0xRektora <13756173+0xRektora@users.noreply.github.com>
2024-05-12 11:38:08 -0400

    feat: New `deployOracleMock__task`

--- tasks/scopes/deployScope.ts
+++ tasks/scopes/deployScope.ts
@@ -2,6 +2,7 @@ import '@nomiclabs/hardhat-ethers';
 import { scope, types } from 'hardhat/config';
 import { TAP_TASK } from 'tapioca-sdk';
 import { deployFormToken__task } from 'tasks/deploy/deployFormToken';
+import { deployOracleMock__task } from 'tasks/deploy/deployOracleMock';
 
 const deployScope = scope('deploys', 'Deployment tasks');
 
@@ -15,4 +16,15 @@ TAP_TASK(
             18,
             types.int,
         ),
+);
+
+TAP_TASK(
+    deployScope
+        .task(
+            'oracleMock',
+            'Deploy OracleMock contract.',
+            deployOracleMock__task,
+        )
+        .addParam('name', 'The name of the oracle.')
+        .addParam('rate', 'Rate rate, in ether (ex: "1.2" for 1.2e18).'),
 );
