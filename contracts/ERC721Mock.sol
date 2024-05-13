77569e25e51201fdd04a8716df1423d620e0c284
0xRektora <13756173+0xRektora@users.noreply.github.com>
2024-05-12 23:32:39 -0400

    patch(`ERC721Mock`): Add `onlyOwner` minting

--- contracts/ERC721Mock.sol
+++ contracts/ERC721Mock.sol
@@ -2,13 +2,16 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ERC721Mock is ERC721 {
+contract ERC721Mock is ERC721, Ownable {
     uint256 private _tokenIds;
 
-    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
+    constructor(string memory _name, string memory _symbol, address _owner) ERC721(_name, _symbol) {
+        transferOwnership(_owner);
+    }
 
-    function mint(address to) public returns (uint256) {
+    function mint(address to) public onlyOwner returns (uint256) {
         _tokenIds += 1;
         _mint(to, _tokenIds);
         return _tokenIds;
